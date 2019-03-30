8939a878b7d8499f85469d3c7ad0fe2537445bde
Wilian Pantoja <wilian.pantoja@sunsetswh.com>
2019-03-29 21:35:40 -0500

    ...

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php 
 header('Access-Control-Allow-Origin: *');
-
 header('Access-Control-Allow-Methods: GET, POST');
-
 header("Access-Control-Allow-Headers: X-Requested-With");
+header('Content-Type: application/json');
 
 $curl = curl_init();
 
